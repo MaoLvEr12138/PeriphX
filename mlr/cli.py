4ca628be15685ba3644be4a7a8ed7d520287ed70
海蜇 <156986934+GuoHaiZhe12138@users.noreply.github.com>
2026-06-24 20:58:19 +0800

    mlr read yaml successfully

--- mlr/cli.py
+++ mlr/cli.py
@@ -1,12 +1,21 @@
 import typer
+import yaml
+from pathlib import Path
 
 app = typer.Typer()
 
 @app.command()
 def validate():
-    print("validate")
+    with open(Path(__file__).resolve().parent.parent / "tests" / "manifest.yaml", "r", encoding="utf-8") as file:
+        data = yaml.safe_load(file)
+
+    print(data)
 
 
 @app.command()
 def build():
     print("build")
+
+
+if __name__ == "__main__":
+    app()
